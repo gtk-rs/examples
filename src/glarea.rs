9964286cf5b722a8693e78b0f704034cf1115271
Maxwell Koo <mjkoo90@gmail.com>
2015-11-03 18:54:47 -0500

    Render a triangle with epoxy

--- src/glarea.rs
+++ src/glarea.rs
@@ -6,13 +6,70 @@ extern crate gtk;
 extern crate libc;
 
 #[cfg(feature = "opengl")]
+extern crate epoxy;
+
+// Based off of the static-triangle example from gl-rs
+#[cfg(feature = "opengl")]
 mod example {
-    extern crate epoxy;
+    use std::ffi;
+    use std::mem;
+    use std::ptr;
 
     use gtk;
     use gtk::traits::*;
     use gtk::signal::Inhibit;
     use gtk::{GLArea, Window};
+
+    use epoxy;
+    use epoxy::types::*;
+    use epoxy::Gl;
+
+    fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+        let shader;
+        unsafe {
+            shader = Gl.CreateShader(ty);
+            // Attempt to compile the shader
+            let csrc = ffi::CString::new(src).unwrap().as_ptr();
+            Gl.ShaderSource(shader, 1, &csrc, ptr::null());
+            Gl.CompileShader(shader);
+
+            // Get the compile status
+            let mut status = epoxy::FALSE as GLint;
+            Gl.GetShaderiv(shader, epoxy::COMPILE_STATUS, &mut status);
+
+            // Fail on error
+            if status != (epoxy::TRUE as GLint) {
+                let mut len = 0;
+                Gl.GetShaderiv(shader, epoxy::INFO_LOG_LENGTH, &mut len);
+                let mut buf = vec![0u8; len as usize - 1];
+                Gl.GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+                panic!("Error compiling shader");
+            }
+        }
+        shader
+    }
+
+    fn link_program(vs: GLuint, fs: GLuint) -> GLuint { unsafe {
+        let program = Gl.CreateProgram();
+        Gl.AttachShader(program, vs);
+        Gl.AttachShader(program, fs);
+        Gl.LinkProgram(program);
+
+        // Get the link status
+        let mut status = epoxy::FALSE as GLint;
+        Gl.GetProgramiv(program, epoxy::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (epoxy::TRUE as GLint) {
+            let mut len: GLint = 0;
+            Gl.GetProgramiv(program, epoxy::INFO_LOG_LENGTH, &mut len);
+            let mut buf = vec![0u8; len as usize - 1];
+            Gl.GetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            panic!("Error linking shader");
+        }
+
+        program
+    }}
 
     pub fn main() {
         if gtk::init().is_err() {
@@ -29,11 +86,58 @@ mod example {
         });
 
         glarea.connect_render(|_, _| {
-            unsafe {
-                epoxy::Gl.ClearColor(1.0, 0.0, 0.0, 1.0);
-                epoxy::Gl.Clear(epoxy::COLOR_BUFFER_BIT);
+            // TODO: Most of this should be done on realize, need to figure out issues with the
+            // context then though
+            let vertices: [GLfloat; 6] = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
 
-                epoxy::Gl.Flush();
+            let vert_shader_src = r#"
+                #version 140
+
+                in vec2 position;
+
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0);
+                }"#;
+
+            let frag_shader_src = r#"
+                #version 140
+
+                out vec4 color;
+
+                void main() {
+                    color = vec4(1.0, 0.0, 0.0, 1.0);
+                }"#;
+
+            let vs = compile_shader(vert_shader_src, epoxy::VERTEX_SHADER);
+            let fs = compile_shader(frag_shader_src, epoxy::FRAGMENT_SHADER);
+            let program = link_program(vs, fs);
+
+            let mut vao: GLuint = 0;
+            let mut vbo: GLuint = 0;
+
+            unsafe {
+                Gl.GenVertexArrays(1, &mut vao);
+                Gl.BindVertexArray(vao);
+
+                Gl.GenBuffers(1, &mut vbo);
+                Gl.BindBuffer(epoxy::ARRAY_BUFFER, vbo);
+                Gl.BufferData(epoxy::ARRAY_BUFFER,
+                              (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                              mem::transmute(&vertices[0]),
+                              epoxy::STATIC_DRAW);
+
+                Gl.UseProgram(program);
+                Gl.BindFragDataLocation(program, 0, ffi::CString::new("color").unwrap().as_ptr());
+
+                let pos_attr = Gl.GetAttribLocation(program, ffi::CString::new("position").unwrap().as_ptr());
+                Gl.EnableVertexAttribArray(pos_attr as GLuint);
+                Gl.VertexAttribPointer(pos_attr as GLuint, 2, epoxy::FLOAT,
+                                       epoxy::FALSE as GLboolean, 0, ptr::null());
+
+                Gl.ClearColor(0.3, 0.3, 0.3, 1.0);
+                Gl.Clear(epoxy::COLOR_BUFFER_BIT);
+
+                Gl.DrawArrays(epoxy::TRIANGLES, 0, 3);
             };
 
             Inhibit(false)
