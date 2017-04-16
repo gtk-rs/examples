ca80361dbb7798cb909ecde03be797fe6d30faef
Stefan Müller <s.mueller@it.kls-glt.de>
2017-04-16 14:36:11 +0200

    Add some comment

--- src/radiobutton.rs
+++ src/radiobutton.rs
@@ -26,39 +26,37 @@ fn main() {
         Inhibit(false)
     });
 
-    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 10);
 
-    // All RadioButton for themselves
+    // RadioButton "stand alone"
+    // Each Radio Button is independent of each other.
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
     let radio_size_10 = gtk::RadioButton::new_with_label_from_widget(None, "10x10");
     let radio_size_20 = gtk::RadioButton::new_with_label_from_widget(None, "20x20");
     let radio_size_30 = gtk::RadioButton::new_with_label_from_widget(None, "30x30");
     let radio_size_40 = gtk::RadioButton::new_with_label_from_widget(None, "40x40");
 
-    vbox.pack_start(&gtk::Label::new("All RadioButton for themselves"), true, false, 0);
+    vbox.pack_start(&gtk::Label::new("RadioButton stand alone"), true, false, 0);
     vbox.pack_start(&radio_size_10, false, false, 0);
     vbox.pack_start(&radio_size_20, false, false, 0);
     vbox.pack_start(&radio_size_30, false, false, 0);
     vbox.pack_start(&radio_size_40, false, false, 0);
 
-    let radio_button_group = vec![radio_size_10, radio_size_20, radio_size_30, radio_size_40];
-
     hbox.pack_start(&vbox, false, false, 0);
 
-    // RadioButton child of RadioButton `10x10`
+    // RadioButton child of `10x10`
+    // Here all Radio Button are connected to each other. Only one can be set at once.
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
     let radio_size_10 = gtk::RadioButton::new_with_label_from_widget(None, "10x10");
     let radio_size_20 = gtk::RadioButton::new_with_label_from_widget(Some(&radio_size_10), "20x20");
     let radio_size_30 = gtk::RadioButton::new_with_label_from_widget(Some(&radio_size_10), "30x30");
     let radio_size_40 = gtk::RadioButton::new_with_label_from_widget(Some(&radio_size_10), "40x40");
 
-    vbox.pack_start(&gtk::Label::new("RadioButton child of RadioButton `10x10`"), true, false, 0);
+    vbox.pack_start(&gtk::Label::new("RadioButton child of `10x10`"), true, false, 0);
     vbox.pack_start(&radio_size_10, false, false, 0);
     vbox.pack_start(&radio_size_20, false, false, 0);
     vbox.pack_start(&radio_size_30, false, false, 0);
     vbox.pack_start(&radio_size_40, false, false, 0);
-
-    let radio_button_group = vec![radio_size_10, radio_size_20, radio_size_30, radio_size_40];
 
     hbox.pack_end(&vbox, false, false, 0);
     window.add(&hbox);
