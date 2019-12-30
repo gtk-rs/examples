b75fc8b4a884ff98e5ffa4268e718ea01722b800
artonio <asukhovatkin@gmail.com>
2019-12-30 09:49:17 -0500

    cleanup and comments

--- src/bin/iconview_example.rs
+++ src/bin/iconview_example.rs
@@ -11,11 +11,19 @@ use gio::prelude::*;
 use gtk::prelude::*;
 
 use std::env::args;
+use std::process;
+
+enum IconViewColumnType {
+    TextColumn = 0,
+    PixbufColumn = 1
+}
 
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
 
-    window.set_title("Fucking IconView In Rust");
+    let icons: [&'static str; 3] = ["edit-cut", "edit-paste", "edit-copy"];
+
+    window.set_title("IconView Example");
     window.set_border_width(10);
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(350, 70);
@@ -30,20 +38,31 @@ fn build_ui(application: &gtk::Application) {
     let col_types: [glib::Type; 2] = [glib::Type::String, gdk_pixbuf::Pixbuf::static_type()];
     let icon_view_model = gtk::ListStore::new(&col_types);
 
-    // icon_view_model.append();
     icon_view.set_model(Some(&icon_view_model));
-    icon_view.set_text_column(0);
-    icon_view.set_pixbuf_column(1);
+    icon_view.set_text_column(IconViewColumnType::TextColumn as i32);
+    icon_view.set_pixbuf_column(IconViewColumnType::PixbufColumn as i32);
 
     let icon = gtk::IconTheme::get_default();
     if let Some(i) = icon {
-        let result = i.load_icon("edit-cut", 64, gtk::IconLookupFlags::empty());
-        match result {
-            Ok(r) => {
-                icon_view_model.insert_with_values(None, &[0, 1], &[&String::from("test"), &r]);
-            },
-            Err(e) => {}
+        for x in &icons {
+            let result = i.load_icon(x, 64, gtk::IconLookupFlags::empty());
+            match result {
+                Ok(r) => {
+                    /* arguments are position: 
+                     Option<u32>,
+                     columns: &[u32],
+                     values: &[&dyn ToValue]
+                     Columns are first defined in icon_view.set_text_column and icon_view.set_pixbuf_column
+                    */
+                    icon_view_model.insert_with_values(None, &[IconViewColumnType::TextColumn as u32, IconViewColumnType::PixbufColumn as u32], &[&String::from("Label"), &r]);
+                },
+                Err(err) => {
+                    println!("Error: {}", err);
+                    process::exit(1);
+                }
+            }
         }
+
     }
     
     window.add(&icon_view);
@@ -52,7 +71,7 @@ fn build_ui(application: &gtk::Application) {
 
 fn main() {
     let application =
-        gtk::Application::new(Some("com.github.gtk-rs.examples.basic"), Default::default())
+        gtk::Application::new(Some("com.github.gtk-rs.examples.iconview_example"), Default::default())
             .expect("Initialization failed...");
 
     application.connect_activate(|app| {
